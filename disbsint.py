7a5613aa7baff7bc6033dd6966b40d12559a2b56
titan-rocky <rockalbert466@gmail.com>
2021-09-10 14:10:15 +0530

    disc_bs integration

---
+++ disbsint.py
@@ -0,0 +1,29 @@
+import csv
+
+def add_entry(dis_id,bs_tag):
+	file=open('disbs.csv','a',newline='')
+	wr=csv.writer(file)
+	wr.writerow([dis_id,bs_tag])
+	file.close()
+
+def show_entries():
+	file=open('disbs.csv','r',newline='')
+	wr=csv.reader(file)
+	b=[i for i in wr]
+	file.close()
+	return b
+
+def retr_entry(**data):
+	file=open('disbs.csv','a',newline='')
+	wr=csv.reader(file)
+	b=[i for i in wr]
+	file.close()
+	if 'dis_id' in data:
+		for i in range(len(b)):
+			if dis_id==i[0]:
+				return i[1]
+	if 'bs_tag' in data:
+		for i in range(len(b)):
+			if bs_tag==i[1]:
+				return i[0]
+	
