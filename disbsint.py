2e16e1cae0ef587a7a01fdf961d9be788d787a20
titan-rocky <rockalbert466@gmail.com>
2021-09-10 16:46:43 +0530

    Update disbsint.py

--- disbsint.py
+++ disbsint.py
@@ -2,23 +2,28 @@ import csv
 
 def add_entry(dis_id,bs_tag):
 	file=open('disbs.csv','a',newline='')
-  print('file opened')
+	print('disbs.csv opened')
 	wr=csv.writer(file)
 	wr.writerow([dis_id,bs_tag])
 	file.close()
+	print('disbs.csv closed')
 
 def show_entries():
 	file=open('disbs.csv','r',newline='')
 	wr=csv.reader(file)
+	print('disbs.csv opened')
 	b=[i for i in wr]
 	file.close()
+	print('disbs.csv closed')
 	return b
 
 def retr_entry(**data):
 	file=open('disbs.csv','a',newline='')
+	print('disbs.csv opened')
 	wr=csv.reader(file)
 	b=[i for i in wr]
 	file.close()
+	print('disbs.csv closed')
 	if 'dis_id' in data:
 		for i in range(len(b)):
 			if data['dis_id']==i[0]:
