347909a7dda68e0c24cf76c4b98263f1166a27dc
titan-rocky <rockalbert466@gmail.com>
2021-09-11 23:57:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -116,7 +116,9 @@ async def on_message(message):
 async def on_command(ctx):
 	webhook = Webhook.from_url('https://discord.com/api/webhooks/886310128838647838/nlrVxhGd_J8PHZs0hxaLMQ_4qUQBF3nMmZfLoIKjURWh_iYLMmlAxP-aTRmXs4Jd1ilq',adapter=RequestsWebhookAdapter())
 	col=[0xf8b195,0xf67280,0xc06c84,0x6c5b7b,0x355c7d]
-	dd=discord.Embed(color=random.choice(col),title="Command Logs",desc=f'**{ctx.command.name}**\n{ctx.args}',footer=f'invoked by {ctx.author.name}{ctx.author.discriminator}')
+	dd=discord.Embed(color=random.choice(col),title="Command Logs",desc='ss')
+	dd.add_field(name=f'{ctx.command.name}',value='Arguments : {ctx.args}')
+	dd.set.footer(text=f'invoked by {ctx.author.name}{ctx.author.discriminator}')
 	webhook.send(embed=dd)
 	print(f'{ctx.message.content} invoked by {ctx.author.id}{ctx.author.discriminator}')
 	    
