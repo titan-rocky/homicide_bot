bf6a33b17cf7d21d9e4cd247f7b78e3efdd8ba00
titan-rocky <rockalbert466@gmail.com>
2021-09-11 11:28:47 +0530

    discord.client to discord.ext.commands
    a Revolution

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ op_discord_id=['557914347490508806','709740580988780624']
 
 bs_APIKEY=os.environ['BRAWL_API_KEY']
 
-cl=discord.client.Client()
+cl=commands.bot(command_prefix='belle ',description='Dedicated for HOMICIDE_CREW')
 
 
 @cl.event
@@ -28,65 +28,14 @@ async def on_ready():
 	cl.loop.create_task(club_entry())
 	cl.loop.create_task(role_update())
 
-
-
-@cl.event
-async def on_message(message):
-
-	# $hi command
-	if message.content.startswith('$hi'):
-		print(f'$hi by {message.author.name}#{message.author.discriminator} id {message.author.id}')
-		if str(message.author.id)=='557914347490508806':
-			await message.channel.send('Hello m\'Boss')
-		elif str(message.author.id)=='738652832340901939':
-			await message.channel.send('A Warm Welcome , Lord Guardian')
-		elif str(message.author.id)=='709740580988780624':
-			await message.channel.send('Hello Guardian')	
-		else:
-			await message.channel.send(f'Hello ,{message.author.name} ')
-
-	# $addit command - discord_brawl stars id integration
-	if message.content.startswith('$addid'):
-		print(f'$addid requested by {message.author.name}#{message.author.discriminator} id {message.author.id}')
-		if str(message.author.id) in op_discord_id:
-			b=message.content
-			b=b.lstrip('$addid ').split(' ')
-			print(b)
-			if len(b)>2:
-				await message.channel.send(f'{message.author.mention} , you can add only one entry')
-			elif len(b)!=2:
-				await message.channel.send(f'{message.author.mention} , Please give any entry')
-			elif len(b)==2:
-				await message.channel.send(f'{message.author.mention} , Please Confirm your request by typing \'confirm\' (case sensitive)')
-				def check(m):
-					return m.content=='confirm' and m.channel==message.channel
-
-				try:
-					msg=await cl.wait_for('message',check=check,timeout=30)
-				except asyncio.TimeoutError:
-					await message.channel.send(f'{message.author.mention} Your Request has been declined')
-				else:
-					disbsint.add_entry(b[0],b[1])
-					print(f'added {b[0]}:{b[1]} disbs.csv')
-					await message.channel.send(f'Added ID {b[0]} successfully !')
-		else:
-			await message.channel.send('You are not Authorized 😓')
-
-		# $help - help embed
-		
-
-
-
-
 def header():
 	bs_APIKEY=os.environ['BRAWL_API_KEY']
 	m={'Accept':'application/json','authorization':f'Bearer {bs_APIKEY}'}
 	return m
 
-
-
 @cl.event
 async def role_update():
+	print('role_update() called')
 	b=requests.get(r'https://api.brawlstars.com/v1/clubs/%23202U9UPLU/members',headers=header()) # %23 - js '#
 	dic=b.json()
 	if 'reason' in dic and dic['reason']=='accessDenied.invalidIp':
@@ -108,7 +57,7 @@ async def role_update():
 
 @cl.event
 async def club_entry():
-
+	print('club_entry() called')
 	mo={}
 	while 0:
 		
@@ -142,6 +91,60 @@ async def club_entry():
 		mo=dic
 
 		await asyncio.sleep(60)
+
+
+
+
+@cl.event
+async def on_message(message):
+	if message.author==bot.user:
+		return
+
+	bot.process_commands(message)
+
+	# $hi command
+	# $addit command - discord_brawl stars id integration
+	# $help - help embed
+		
+@bot.command()
+async def hi():
+	print(f'$hi by {ctx.author.name}#{ctx.author.discriminator} id {ctx.author.id}')
+	if str(ctx.author.id)=='557914347490508806':
+		await ctx.channel.send('Hello m\'Boss')
+	elif str(ctx.author.id)=='738652832340901939':
+		await ctx.channel.send('A Warm Welcome , Lord Guardian')
+	elif str(ctx.author.id)=='709740580988780624':
+		await ctx.channel.send('Hello Guardian')	
+	else:
+		await message.channel.send(f'Hello ,{message.author.name} ')
+
+
+@bot.command()
+async def addit():
+	print(f'$addid requested by {ctx.author.name}#{ctx.author.discriminator} id {ctx.author.id}')
+	if str(ctx.author.id) in op_discord_id:
+		b=ctx.message.content
+		b=b.split(' ')
+		print(b)
+		if len(b)>2:
+			await ctx.channel.send(f'{ctx.author.mention} , you can add only one entry')
+		elif len(b)!=2:
+			await ctx.channel.send(f'{ctx.author.mention} , Please give any entry')
+		elif len(b)==2:
+			await ctx.channel.send(f'{ctx.author.mention} , Please Confirm your request by typing \'confirm\' (case sensitive)')
+			def check(m):
+				return m.content=='confirm' and m.channel==ctx.channel
+
+			try:
+				msg=await cl.wait_for('ctx',check=check,timeout=30)
+			except asyncio.TimeoutError:
+				await ctx.channel.send(f'{ctx.author.mention} Your Request has been declined')
+			else:
+				disbsint.add_entry(b[0],b[1])
+				print(f'added {b[0]}:{b[1]} disbs.csv')
+				await ctx.channel.send(f'Added ID {b[0]} successfully !')
+	else:
+		await ctx.channel.send('You are not Authorized 😓')
 
 
 
