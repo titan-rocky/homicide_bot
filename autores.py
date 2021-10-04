b984990cec1dae0a4ff0619df54359d0630df3f9
titan-rocky <rockalbert466@gmail.com>
2021-10-05 00:12:53 +0530

    gfj

--- autores.py
+++ autores.py
@@ -6,23 +6,35 @@ import asyncio
 class AutoResponse(commands.Cog):
 	def __init__(self,bot):
 		self.bot=bot
-		self.gamerec_ch=self.bot.get_channel(737955923158368277)
-		self.map_ch=self.bot.get_channel(737955923158368277)
+		self.gamerec_chid=737955923158368277
+		self.map_chid=769188270247510067
 
 
 	@commands.Cog.listener()
 	async def on_message(self,message):
 		#game-records
-		if message.channel==self.gamerec_ch:
-			print(message.content)
+		if message.channel.id==self.gamerec_chid:
 			for i in ['record!','record !']:
 				if message.content.lower().startswith(i):
-					em1=discord.Partialemoji()
-					await message.add_reaction(em1.from_str('<a:upvote:894641763417981029>'))
+					em1=self.bot.get_emoji(894654840184127518)
+					em2=self.bot.get_emoji(894641763417981029)
+					em3=self.bot.get_emoji(894641763640307732)
+					await message.add_reaction(em1)
+					await message.add_reaction(em2)
+					await message.add_reaction(em3)
+					break
 			else:
 				await message.delete()	
 		#created-maps
-		elif message.channel==self.map_ch:
-			pass
+		elif message.channel.id==self.map_chid:
+			for i in ['new map!','new map !']:
+				if message.content.lower().startswith(i):
+					em1=self.bot.get_emoji(894641763417981029)
+					em2=self.bot.get_emoji(894641763640307732)
+					await message.add_reaction(em1)
+					await message.add_reaction(em2)
+					break
+			else:
+				await message.delete()	
 
 
