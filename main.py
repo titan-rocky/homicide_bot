2fbfa4500805839ad2c7fe1415b3bdfaf0f7a317
titan-rocky <rockalbert466@gmail.com>
2021-10-05 12:17:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -54,8 +54,8 @@ async def gud_mor():
 		await cl.change_presence(status=discord.Status.idle,activity=discord.ActivityType.watching(name='People Saying GM'))
 	elif b.startswith('12:00'):
 		await cl.change_presence(status=discord.Status.idle,activity=discord.ActivityType.watching(name='President Naruto\'s Comeback'))
-	elif b.startswith('12:12'):
-		await cl.change_presence(activity=discord.Spotify(track_id='1J03Vp93ybKIxfzYI4YJtL'))
+	elif b.startswith('18:00'):
+		await cl.change_presence(activity=discord.ActivityType.listening(name='Megalovonia , by Toby Fox'))
 	elif b.startswith('19:30'):
 		await cl.change_presence(activity=discord.Game(name="Brawl Stars"))
 
