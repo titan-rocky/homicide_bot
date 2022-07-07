73c2b1298a2d726a87996cb3a5d2553be7c26d47
titan-rocky <rockalbert466@gmail.com>
2022-07-07 14:42:00 +0530

    Update autores.py

--- autores.py
+++ autores.py
@@ -12,7 +12,7 @@ class AutoResponse(commands.Cog):
 		self.art_chid=992054563064533075
 		self.selfrole_chid=994495593856651334
 		self.homi_roledict={'bs':992616272640618526,'coc':992616363971596370,'cr':992616443361378304,'mc':992616012841222144}
-		self.servers=[691292302580121690]
+		self.servers=[await self.bot.get_guild(691292302580121690)]
 		self.resource_server=[887015707366277170]
 
 
@@ -73,7 +73,7 @@ class AutoResponse(commands.Cog):
 		if payload.channel_id==self.selfrole_chid and not(payload.member.bot):
 			for i in role_emote:
 				if payload.emoji.id==i:
-					await payload.member.add_roles(await self.bot.get_guild(self.servers[0]).get_role(self.homi_roledict[role_emote[i]]))
+					await payload.member.add_roles(servers[0].get_role(self.homi_roledict[role_emote[i]]))
 
 	@commands.Cog.listener()
 	async def on_raw_reaction_remove(self,payload):
@@ -81,7 +81,7 @@ class AutoResponse(commands.Cog):
 		if payload.channel_id==self.selfrole_chid and not(payload.member.bot):
 			for i in role_emote:
 				if payload.emoji.id==i:
-					await payload.member.remove_roles(await self.bot.get_guild(self.servers[0]).get_role(self.homi_roledict[role_emote[i]]))
+					await payload.member.remove_roles(servers[0].get_role(self.homi_roledict[role_emote[i]]))
 
 	
 
